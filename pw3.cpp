e9d074b7cda0bcbaa15eca7311d4b1b27da2af47
Suraj Yadav <114130470+ErSuraj097@users.noreply.github.com>
2023-01-23 13:28:29 +0530

    Add files via upload

---
+++ pw3.cpp
@@ -0,0 +1,103 @@
+//...........Question 1
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+int x,y;
+cout<<"Enter the first number ";
+cin >> x;
+cout<<"Enter the second number ";
+cin >> y;
+cout << (x == y) ? 1:0 ;
+return 0;
+}
+
+//...........Question 2
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout << "Enter the first number : ";
+cin >> a;
+cout << "Enter the second number : ";
+cin >> b;
+cout<< ((a < 50) && (a < b))?1:0;
+return 0;
+}
+
+
+//...........Question 3
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int total, boys, girls;
+boys = 17;
+total = (80*45)/100; //total students getting grade A
+girls = total - boys; //total number of girls getting grade A
+cout << "Number of girls getting grade A = " << girls << endl;
+return 0;
+}
+
+
+
+//...........Question 4
+#include <iostream>
+using namespace std;
+int main()
+{
+int n, first, second, third, fourth, fifth, sum;
+n = 12345;
+/*Take out each digit of this number and then finally add the first and the second last digits
+first = n/10000; //first digit
+n = n%10000;
+second = n/1000; //second digit
+n = n%1000;
+third = n/100; //third digit
+n = n%100;
+fourth = n/10; //fourth digit
+fifth = n%10; //fifth digit
+sum = first + fourth;
+cout << "sum of first and fourth digit is: " << sum << endl;
+return 0;
+}
+
+
+//...........Question 5
+#include <iostream>
+using namespace std;
+int main()
+{
+int n, first, second, third,sum;
+n = 524;
+/*Take out each digit of this number and then finally add the digits
+first = n/100; //first digit
+n = n%100;
+second = n/10; //second digit
+third = n%10; //third digit
+sum = first + second + third;
+cout << "sum of 3 digits is: " << sum << endl;
+return 0;
+}
+
+
+//...........Question 6
+
+# include <iostream>
+using namespace std;
+int main() {
+float num1, num2;
+cout << "Enter the first number ";
+cin >> num1;
+cout << "Enter the second number ";
+cin >> num2;
+cout << "Sum = " << num1 + num2<<endl;
+cout <<"Difference = " << num1 - num2<<endl;
+cout <<"Product = " << num1 * num2<<endl;
+cout << "Division = " << num1 / num2<<endl;
+return 0;
+}
+*/
