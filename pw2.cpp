750a30b687d6fe1b29c1a762cac6c8e62fb7db00
Suraj Yadav <114130470+ErSuraj097@users.noreply.github.com>
2023-01-23 13:17:59 +0530

    Add files via upload

---
+++ pw2.cpp
@@ -0,0 +1,66 @@
+//...........Question 1
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int x = 23;
+int y = 50;
+cout << x*y << endl;
+return 0;
+}*/
+
+//...........Question 2
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<< "ASCII value of u is "<<(int)'U'<<endl;
+}
+
+//...........Question 3
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l=10;
+    int b=20;
+    float area;
+    area=l*b;
+    cout<< "Area    :"<<area<<endl;
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main (){
+    int i ;
+    cout<<"Enter the number:";
+    cin>>i; 
+
+    cout<<"Cube : " <<i*i*i<<endl;
+    return 0;
+
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "\nSize of fundamental data types :\n";
+cout << " The sizeof(char) : " << sizeof(char) << " bytes \n" ;
+cout << " The sizeof(short) : " << sizeof(short) << " bytes \n" ;
+cout << " The sizeof(int) : " << sizeof(int) << " bytes \n" ;
+cout << " The sizeof(long): " << sizeof(long) << " bytes \n" ;
+cout << " The sizeof(long long) :" << sizeof(long long) << " bytes \n";
+cout << " The sizeof(float) :" << sizeof(float) << " bytes \n" ;
+cout << " The sizeof(double) :" << sizeof(double) << " bytes \n";
+cout << " The sizeof(long double) :" << sizeof(long double) << " bytes \n";
+cout << " The sizeof(bool) : " << sizeof(bool) << " bytes \n\n";
+return 0;
+}
